ea08d3ce5e0843e11662f276c12438e853c25e05
YEONSOO <m60kys@gmail.com>
2021-02-04 13:01:43 +0900

    Initial commit of my components without tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React from 'react';
+import Header from "./components/header"
+import "./app.scss"
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       Test
     </div>
   );
