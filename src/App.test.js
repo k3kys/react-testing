c28de70b3d114cf260fccf37987cac6375fa412e
YEONSOO <m60kys@gmail.com>
2021-02-04 18:22:39 +0900

    new tests for connected component

---
+++ src/App.test.js
@@ -0,0 +1,35 @@
+import App from "./App"
+import { shallow } from "enzyme"
+import { findByTestAtrr, testStore } from "../Utils"
+import React from "react"
+
+const setUp = (initialState={}) => {
+    const store = testStore(initialState)
+    const wrapper = shallow(<App store={store}/>).childAt(0).dive()
+    return wrapper
+}
+
+describe("App Component", () => {
+    let wrapper
+    beforeEach(() => {
+        const initialState = {
+            posts: [{
+                title: "Example title1",
+                body: "Some text"
+            }, {
+                title: "Example title2",
+                body: "Some text"
+            }, {
+                title: "Example title3",
+                body: "Some text"
+            }]
+        }
+        
+        wrapper = setUp(initialState)
+    })
+    
+    it("Should render without errors", () => {
+        const component = findByTestAtrr(wrapper, "appComponent")
+        expect(component.length).toBe(1)
+    })
+})
